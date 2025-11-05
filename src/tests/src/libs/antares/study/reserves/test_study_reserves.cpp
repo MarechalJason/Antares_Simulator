31e18870db3ff1648f74c9ab542db3367b8d6833
sylvmara <symarandon@gmail.com>
2025-11-05 14:24:35 +0100

    Added tests for reserves

--- src/tests/src/libs/antares/study/reserves/test_study_reserves.cpp
+++ src/tests/src/libs/antares/study/reserves/test_study_reserves.cpp
@@ -20,7 +20,9 @@
  */
 
 #define BOOST_TEST_MODULE study
-#define WIN32_LEAN_AND_MEAN
+#include <files-system.h>
+#include <filesystem>
+
 #include <boost/test/unit_test.hpp>
 
 #include <antares/solver/simulation/sim_structure_probleme_economique.h>
@@ -28,12 +30,19 @@
 
 using namespace Antares::Data;
 
+void addThermalCluster(Data::Area* area, const std::string& name)
+{
+    auto c = std::make_shared<ThermalCluster>(area);
+    c->setName(name);
+    area->thermal.list.addToCompleteList(c);
+}
+
 /*!
  * Study with one area named "A"
  */
-struct OneProblemWithReserves
+struct OneProblemWithReservesOneArea
 {
-    OneProblemWithReserves()
+    OneProblemWithReservesOneArea()
     {
         study = std::make_unique<Study>();
         areaA = study->areaAdd("A");
@@ -64,6 +73,64 @@ struct OneProblemWithReserves
     CapacityReservation tmpCapacityReservationDown;
 };
 
+/*!
+ * Study with two areas named "A" and "B"
+ */
+struct OneProblemWithReservesTwoAreas
+{
+    OneProblemWithReservesTwoAreas()
+    {
+        study = std::make_unique<Study>();
+        areaA = study->areaAdd("A");
+        areaB = study->areaAdd("B");
+        CAPACITY_RESERVATION areaCapacityReservationsUp;
+        study->parameters.simulationDays.first = 0;
+        study->parameters.simulationDays.end = 7;
+
+        tmpCapacityReservationUp.unsuppliedCost = 1;
+        tmpCapacityReservationUp.referenceActivationHours = 2;
+        tmpCapacityReservationUp.powerActivationRatio = 3;
+        tmpCapacityReservationUp.energyActivationRatio = 4;
+
+        tmpCapacityReservationDown.unsuppliedCost = 5;
+        tmpCapacityReservationDown.referenceActivationHours = 6;
+        tmpCapacityReservationDown.powerActivationRatio = 7;
+        tmpCapacityReservationDown.energyActivationRatio = 8;
+
+        tmpCapacityReservationUpB.unsuppliedCost = 11;
+        tmpCapacityReservationUpB.referenceActivationHours = 12;
+        tmpCapacityReservationUpB.powerActivationRatio = 13;
+        tmpCapacityReservationUpB.energyActivationRatio = 14;
+
+        tmpCapacityReservationDownB.unsuppliedCost = 15;
+        tmpCapacityReservationDownB.referenceActivationHours = 16;
+        tmpCapacityReservationDownB.powerActivationRatio = 17;
+        tmpCapacityReservationDownB.energyActivationRatio = 18;
+
+        areaA->allCapacityReservations = AllCapacityReservations();
+        areaA->allCapacityReservations()
+          .areaCapacityReservationsUp.emplace("ReserveUp", tmpCapacityReservationUp);
+        areaA->allCapacityReservations()
+          .areaCapacityReservationsDown.emplace("ReserveDown", tmpCapacityReservationDown);
+
+        areaB->allCapacityReservations = AllCapacityReservations();
+        areaB->allCapacityReservations()
+          .areaCapacityReservationsUp.emplace("ReserveUp", tmpCapacityReservationUpB);
+        areaB->allCapacityReservations()
+          .areaCapacityReservationsDown.emplace("ReserveDown", tmpCapacityReservationDownB);
+    }
+
+    std::unique_ptr<Study> study;
+    Area* areaA;
+    Area* areaB;
+    PROBLEME_HEBDO problem;
+    CapacityReservation tmpCapacityReservationUp;
+    CapacityReservation tmpCapacityReservationDown;
+
+    CapacityReservation tmpCapacityReservationUpB;
+    CapacityReservation tmpCapacityReservationDownB;
+};
+
 BOOST_AUTO_TEST_SUITE(reserves_operations)
 
 BOOST_AUTO_TEST_CASE(reserve_add)
@@ -82,7 +149,27 @@ BOOST_AUTO_TEST_CASE(reserve_add)
       0);
 }
 
-BOOST_FIXTURE_TEST_CASE(reserve_up, OneProblemWithReserves)
+BOOST_AUTO_TEST_CASE(reserve_add_double)
+{
+    auto study = std::make_unique<Study>();
+    const auto areaA = study->areaAdd("A");
+    CapacityReservation tmpCapacityReservationUp;
+
+    CapacityReservation tmpCapacityReservationUpTwo;
+    areaA->allCapacityReservations = AllCapacityReservations();
+    areaA->allCapacityReservations().areaCapacityReservationsUp.emplace("ReserveUp",
+                                                                        tmpCapacityReservationUp);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().size(), 1);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().contains("ReserveUp"), true);
+    areaA->allCapacityReservations()
+      .areaCapacityReservationsUp.emplace("ReserveUp", tmpCapacityReservationUpTwo);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().size(), 1);
+    areaA->allCapacityReservations()
+      .areaCapacityReservationsUp.emplace("ReserveUpTwo", tmpCapacityReservationUpTwo);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().size(), 2);
+}
+
+BOOST_FIXTURE_TEST_CASE(reserve_one_area, OneProblemWithReservesOneArea)
 {
     BOOST_CHECK_EQUAL(areaA->allCapacityReservations().size(), 2);
     BOOST_CHECK_EQUAL(areaA->allCapacityReservations().areaCapacityReservationsDown.size(), 1);
@@ -119,6 +206,63 @@ BOOST_FIXTURE_TEST_CASE(reserve_up, OneProblemWithReserves)
                         .areaCapacityReservationsDown.at("ReserveDown")
                         .energyActivationRatio,
                       8);
+}
+
+BOOST_FIXTURE_TEST_CASE(reserve_up_two_areas, OneProblemWithReservesTwoAreas)
+{
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().size(), 2);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().areaCapacityReservationsDown.size(), 1);
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations().areaCapacityReservationsUp.size(), 1);
+    BOOST_CHECK_EQUAL(areaB->allCapacityReservations().size(), 2);
+    BOOST_CHECK_EQUAL(areaB->allCapacityReservations().areaCapacityReservationsDown.size(), 1);
+    BOOST_CHECK_EQUAL(areaB->allCapacityReservations().areaCapacityReservationsUp.size(), 1);
+    BOOST_CHECK_EQUAL(
+      areaA->allCapacityReservations().areaCapacityReservationsUp.at("ReserveUp").unsuppliedCost,
+      1);
+
+    BOOST_CHECK_EQUAL(areaA->allCapacityReservations()
+                        .areaCapacityReservationsDown.at("ReserveDown")
+                        .unsuppliedCost,
+                      5);
+
+    BOOST_CHECK_EQUAL(
+      areaB->allCapacityReservations().areaCapacityReservationsUp.at("ReserveUp").unsuppliedCost,
+      11);
+    BOOST_CHECK_EQUAL(areaB->allCapacityReservations()
+                        .areaCapacityReservationsDown.at("ReserveDown")
+                        .unsuppliedCost,
+                      15);
+}
+
+BOOST_FIXTURE_TEST_CASE(test_loadReserveParticipations_Normal, OneProblemWithReservesOneArea)
+{
+    auto studyPath = CREATE_TMP_DIR_BASED_ON_TEST_NAME();
+    addThermalCluster(areaA, "cluster1");
+    addThermalCluster(areaA, "cluster2");
+
+    std::ofstream file(studyPath / "myreserve.ini");
+    file << "[ReserveUp]\n";
+    file << "cluster-name = cluster1\n";
+    file << "max-power = 9.9\n";
+    file << "participation-cost = 8.8\n";
+    file << "max-power-off = 7.7\n";
+    file << "participation-cost-off = 6.6\n";
+    file.close();
+    areaA->thermal.list.loadReserveParticipations(*areaA, studyPath / "myreserve.ini");
+    BOOST_CHECK(
+      areaA->thermal.list.findInAll("cluster1")->reserveParticipationContainer.has_value());
+    BOOST_CHECK(
+      !areaA->thermal.list.findInAll("cluster2")->reserveParticipationContainer.has_value());
+    BOOST_CHECK(areaA->thermal.list.findInAll("cluster1")
+                  ->reserveParticipationContainer.value()
+                  .isParticipatingInReserve("ReserveUp"));
+    BOOST_CHECK(!areaA->thermal.list.findInAll("cluster1")
+                   ->reserveParticipationContainer.value()
+                   .isParticipatingInReserve("ReserveDown"));
+    BOOST_CHECK_EQUAL(areaA->thermal.list.findInAll("cluster1")
+                        ->reserveParticipationContainer.value()
+                        .reserveMaxPower("ReserveUp"),
+                      9.9);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // version
