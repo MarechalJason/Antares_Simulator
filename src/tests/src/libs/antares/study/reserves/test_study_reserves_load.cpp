36c7c84af11a3c14f6ab9b928e99b4aef973ee26
sylvmara <symarandon@gmail.com>
2025-12-09 12:14:01 +0100

    Divided tests into two files

--- src/tests/src/libs/antares/study/reserves/test_study_reserves_load.cpp
+++ src/tests/src/libs/antares/study/reserves/test_study_reserves_load.cpp
@@ -20,7 +20,6 @@
  */
 
 #define BOOST_TEST_MODULE study
-#include <algorithm>
 #include <files-system.h>
 #include <filesystem>
 #include <unit_test_utils.h>
@@ -29,7 +28,6 @@
 #include <boost/test/unit_test_suite.hpp>
 
 #include <antares/logs/logs.h>
-#include <antares/solver/simulation/forTestsOnlySimCalculEco.h>
 #include <antares/solver/simulation/sim_structure_probleme_economique.h>
 #include <antares/solver/simulation/simulation.h>
 #include "antares/study/area/forTestsOnlyList.h"
@@ -209,7 +207,7 @@ struct OneProblemWithReservesTwoAreas
     CapacityReservation tmpCapacityReservationDownB;
 };
 
-BOOST_AUTO_TEST_SUITE(reserves_operations)
+BOOST_AUTO_TEST_SUITE(reserves_operations_load)
 
 BOOST_AUTO_TEST_CASE(reserve_add)
 {
@@ -1417,158 +1415,6 @@ BOOST_FIXTURE_TEST_CASE(test_readReserve_duplicated, OneProblemWithoutReservesOn
     BOOST_CHECK_EQUAL(getErrors().size(), 1);
     BOOST_CHECK_EQUAL(getWarnings().size(), 0);
     BOOST_CHECK(getErrors().contains("A : reserve name already exists for reserve ReserveUp"));
-}
-
-BOOST_FIXTURE_TEST_CASE(test_importCapacityReservation_allGood, OneProblemWithReservesTwoAreas)
-{
-    auto studyPath = CREATE_TMP_DIR_BASED_ON_TEST_NAME();
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.has_value(), false);
-    importCapacityReservations(study->areas, *problemeHebdo);
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.has_value(), true);
-    int indexA = study->areas.find("a")->index;
-    int indexB = study->areas.find("b")->index;
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.value()[indexA].areaCapacityReservations.size(),
-                      2);
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.value()[indexB].areaCapacityReservations.size(),
-                      2);
-    bool containsUp = false;
-    bool containsDown = false;
-    for (auto& reserve: problemeHebdo->allReserves.value()[indexA].areaCapacityReservations)
-    {
-        if (reserve.type == Antares::Data::ReserveType::UP)
-        {
-            BOOST_CHECK_EQUAL(reserve.unsuppliedCost, 1);
-            BOOST_CHECK_EQUAL(reserve.referenceActivationDuration, 2);
-            BOOST_CHECK_EQUAL(reserve.powerActivationRatio, 3);
-            BOOST_CHECK_EQUAL(reserve.energyActivationRatio, 4);
-            containsUp = true;
-        }
-        else
-        {
-            BOOST_CHECK_EQUAL(reserve.unsuppliedCost, 5);
-            BOOST_CHECK_EQUAL(reserve.referenceActivationDuration, 6);
-            BOOST_CHECK_EQUAL(reserve.powerActivationRatio, 7);
-            BOOST_CHECK_EQUAL(reserve.energyActivationRatio, 8);
-            containsDown = true;
-        }
-    }
-    BOOST_CHECK_EQUAL(containsDown, true);
-    BOOST_CHECK_EQUAL(containsUp, true);
-    containsUp = false;
-    containsDown = false;
-
-    for (auto& reserve: problemeHebdo->allReserves.value()[indexB].areaCapacityReservations)
-    {
-        if (reserve.type == Antares::Data::ReserveType::UP)
-        {
-            BOOST_CHECK_EQUAL(reserve.unsuppliedCost, 11);
-            BOOST_CHECK_EQUAL(reserve.referenceActivationDuration, 12);
-            BOOST_CHECK_EQUAL(reserve.powerActivationRatio, 13);
-            BOOST_CHECK_EQUAL(reserve.energyActivationRatio, 14);
-            containsUp = true;
-        }
-        else
-        {
-            BOOST_CHECK_EQUAL(reserve.unsuppliedCost, 15);
-            BOOST_CHECK_EQUAL(reserve.referenceActivationDuration, 16);
-            BOOST_CHECK_EQUAL(reserve.powerActivationRatio, 17);
-            BOOST_CHECK_EQUAL(reserve.energyActivationRatio, 18);
-            containsDown = true;
-        }
-    }
-    BOOST_CHECK_EQUAL(containsDown, true);
-    BOOST_CHECK_EQUAL(containsUp, true);
-}
-
-BOOST_FIXTURE_TEST_CASE(test_importHydroReserves, OneProblemWithReservesTwoAreas)
-{
-    auto studyPath = CREATE_TMP_DIR_BASED_ON_TEST_NAME();
-
-    std::ofstream file(studyPath / "myreserveA.ini");
-    file << "[ReserveUp]\n";
-    file << "participation-cost = 1.1\n";
-    file << "max-store = 2.2\n";
-    file << "max-release = 3.3\n";
-    file << "\n";
-    file << "[ReserveDown]\n";
-    file << "participation-cost = 4.4\n";
-    file << "max-store = 5.5\n";
-    file << "max-release = 6.6\n";
-    file.close();
-    areaA->hydro.loadReserveParticipations(*areaA, studyPath / "myreserveA.ini");
-
-    std::ofstream fileB(studyPath / "myreserveB.ini");
-    fileB << "[ReserveUp]\n";
-    fileB << "participation-cost = 9.9\n";
-    fileB << "max-store = 8.8\n";
-    fileB << "max-release = 7.7\n";
-    fileB.close();
-    areaA->hydro.loadReserveParticipations(*areaB, studyPath / "myreserveB.ini");
-
-    importCapacityReservations(study->areas, *problemeHebdo);
-    importHydroReserves(study->areas, *problemeHebdo);
-    int indexA = study->areas.find("a")->index;
-    int indexB = study->areas.find("b")->index;
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.value()[indexA].areaCapacityReservations.size(),
-                      2);
-    BOOST_CHECK_EQUAL(problemeHebdo->allReserves.value()[indexB].areaCapacityReservations.size(),
-                      2);
-    int maxGlobalHydroParticipationIndex;
-    bool containsUp = false;
-    bool containsDown = false;
-    for (auto& reserve: problemeHebdo->allReserves.value()[indexA].areaCapacityReservations)
-    {
-        BOOST_CHECK_EQUAL(reserve.AllHydroReservesParticipation.size(), 1);
-        auto& part = reserve.AllHydroReservesParticipation[0];
-        if (reserve.type == Antares::Data::ReserveType::UP)
-        {
-            BOOST_CHECK_EQUAL(part.participationCost, 1.1);
-            BOOST_CHECK_EQUAL(part.maxStore, 2.2);
-            BOOST_CHECK_EQUAL(part.maxRelease, 3.3);
-            maxGlobalHydroParticipationIndex = std::max(maxGlobalHydroParticipationIndex,
-                                                        part.globalIndexClusterParticipation);
-            containsUp = true;
-        }
-        else
-        {
-            BOOST_CHECK_EQUAL(part.participationCost, 4.4);
-            BOOST_CHECK_EQUAL(part.maxStore, 5.5);
-            BOOST_CHECK_EQUAL(part.maxRelease, 6.6);
-            maxGlobalHydroParticipationIndex = std::max(maxGlobalHydroParticipationIndex,
-                                                        part.globalIndexClusterParticipation);
-            containsDown = true;
-        }
-    }
-    BOOST_CHECK_EQUAL(containsDown, true);
-    BOOST_CHECK_EQUAL(containsUp, true);
-    containsUp = false;
-    containsDown = false;
-    for (auto& reserve: problemeHebdo->allReserves.value()[indexB].areaCapacityReservations)
-    {
-        if (reserve.AllHydroReservesParticipation.size() == 1)
-        {
-            auto& part = reserve.AllHydroReservesParticipation[0];
-            if (reserve.type == Antares::Data::ReserveType::UP)
-            {
-                BOOST_CHECK_EQUAL(part.participationCost, 9.9);
-                BOOST_CHECK_EQUAL(part.maxStore, 8.8);
-                BOOST_CHECK_EQUAL(part.maxRelease, 7.7);
-                maxGlobalHydroParticipationIndex = std::max(maxGlobalHydroParticipationIndex,
-                                                            part.globalIndexClusterParticipation);
-                containsUp = true;
-            }
-            else if (reserve.type == Antares::Data::ReserveType::DOWN)
-            {
-                containsDown = true;
-            }
-        }
-        else
-        {
-            BOOST_CHECK_EQUAL(reserve.AllHydroReservesParticipation.size(), 0);
-        }
-    }
-    BOOST_CHECK_EQUAL(containsDown, false);
-    BOOST_CHECK_EQUAL(containsUp, true);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // version
