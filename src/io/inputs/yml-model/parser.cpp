27e9f2e2d84728eb3162797bbdfe85349aceb872
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2026-02-27 12:51:55 +0100

    upd

--- src/io/inputs/yml-model/parser.cpp
+++ src/io/inputs/yml-model/parser.cpp
@@ -9,23 +9,90 @@
 
 namespace Antares::IO::Inputs::YmlModel
 {
-void tagNodes(YAML::Node& root)
+
+struct NodeItem
 {
-    if (auto parentId = root["id"]; parentId.IsDefined() && !parentId.IsNull())
+    YAML::Node& node;
+    std::string name;
+};
+
+void tagNodes(NodeItem& nodeItem);
+
+void visitMap(NodeItem& nodeItem)
+{
+    std::string id;
+
+    if (auto parentId = nodeItem.node["id"]; parentId.IsDefined() && !parentId.IsNull())
     {
-        const auto id = parentId.as<std::string>();
-        for (auto child: root)
+        id = parentId.as<std::string>() + "->";
+    }
+    else
+    {
+        id = nodeItem.name + "->";
+    }
+    for (auto it = nodeItem.node.begin(); it != nodeItem.node.end(); ++it)
+    {
+        YAML::Node child = it->second;
+
+        if (!id.empty())
         {
             child.SetTag(id);
-            tagNodes(child);
         }
+        NodeItem childItem{child, it->first.as<std::string>()};
+        tagNodes(childItem);
+    }
+}
+
+void visitSequence(NodeItem& nodeItem)
+{
+    std::string id;
+
+    if (auto parentId = nodeItem.node["id"]; parentId.IsDefined() && !parentId.IsNull())
+    {
+        id = parentId.as<std::string>() + "->";
+    }
+    else
+    {
+        id = nodeItem.name + "->";
+    }
+    for (std::size_t i = 0; i < nodeItem.node.size(); ++i)
+    {
+        YAML::Node child = nodeItem.node[i];
+
+        if (!id.empty())
+        {
+            child.SetTag(id);
+        }
+        NodeItem childItem{child, std::to_string(i)};
+        tagNodes(childItem);
+    }
+}
+
+void tagNodes(NodeItem& nodeItem)
+{
+    if (!nodeItem.node || nodeItem.node.IsNull())
+    {
+        return;
+    }
+
+    if (nodeItem.node.IsMap())
+    {
+        visitMap(nodeItem);
+    }
+    else if (nodeItem.node.IsSequence())
+    {
+        visitSequence(nodeItem);
     }
 }
 
 Library Parser::parse(const std::string& content)
 {
     YAML::Node root = YAML::Load(content);
-    tagNodes(root);
+    auto libraryNode = root["library"];
+    if (libraryNode.IsDefined() && !libraryNode.IsNull())
+    {
+        tagNodes(libraryNode);
+    }
     Library library = root["library"].as<Library>();
 
     return library;
